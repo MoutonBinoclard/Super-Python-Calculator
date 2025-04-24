a2aacbb6f6fd37eb480afbce95c9a554340069aa
Mouton Binoclard <oscarbarral05@gmail.com>
2025-04-24 17:46:31 +0200

    Modified the base parameter

--- SPC.py
+++ SPC.py
@@ -131,13 +131,13 @@ print("")
 'CHANGE THE NAME ON THE GRAPH'
 
 # Here, you can change the tournament name that will appear on the graph.
-nom_du_tournoi = "Melee Only"
+nom_du_tournoi = "SPC V5 BABY !!!!!"
 
 # Here you can specify if you want to add a logotype to the graph
 logo=True
 
 # This is where you can change the path to the logo file:
-logo_path = "SPC_logo/melee_only.png"
+logo_path = "SPC_logo/spc_v5.png"
 
 # Size of the logo
 zoom_logo=0.20
@@ -161,7 +161,7 @@ print("")
 'CHANGE THE STYLE'
 
 # You can change the colors of the graph by modifying the values in the file "SPC_colors_config.json".
-color_scheme = "SPC_color_schemes/melee_only.json"
+color_scheme = "SPC_color_schemes/v5.json"
 
 # If you want to use a custom font, set the following value to True:
 add_custom_fonts = True
