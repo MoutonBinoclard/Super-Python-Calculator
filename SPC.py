3d299f5161f8871ae3d8be51d15d156cd99ec5c6
MoutonBinoclard <oscarbarral05@gmail.com>
2025-08-06 14:04:15 +0200

    remove show entrie

--- SPC.py
+++ SPC.py
@@ -71,7 +71,6 @@ fusion_dict = remove_non_player(fusion_dict)
 from subcode.fusion_dict import sort_fusion_dict
 fusion_dict = sort_fusion_dict(fusion_dict)
 
-show_entries_from_base_dict(base_dict)
 # All the calculations are done
 
 from subcode.export_launch import launch_exportations
