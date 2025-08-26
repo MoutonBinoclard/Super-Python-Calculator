e475c66fb4fc4b052f4b0813526e44670dff59ef
MoutonBinoclard <oscarbarral05@gmail.com>
2025-08-26 22:38:57 +0200

    best graph

--- subcode/export_spreadsheet.py
+++ subcode/export_spreadsheet.py
@@ -19,26 +19,31 @@ def export_spreadsheet_from_csv(csv_path, tournament_name, cs, logo, logo_path, 
     headers = data[0]
     rows = data[1:]
 
-    # Fixed dimensions, no margin
-    row_height = 0.3  # Fixed height per row in inches
-    col_width = 0.8   # Fixed width per column in inches
+    # Fixed dimensions
+    row_height = 0.3  # inches
+    col_width = 0.8   # inches
     
-    fig_height = (len(rows) + 1) * row_height  # +1 for header
+    fig_height = (len(rows) + 1) * row_height
     fig_width = len(headers) * col_width
-    
-    fig, ax = plt.subplots(figsize=(fig_width, fig_height))
+
+    # Use add_axes to force the axes to occupy the full figure
+    fig = plt.figure(figsize=(fig_width, fig_height))
+    ax = fig.add_axes([0, 0, 1, 1])  
     ax.axis('off')
     
     # Create table
-    table = ax.table(cellText=rows, colLabels=headers, 
-                    bbox=[0, 0, 1, 1], cellLoc='center')
+    table = ax.table(
+        cellText=rows, 
+        colLabels=headers, 
+        bbox=[0, 0, 1, 1], 
+        cellLoc='center'
+    )
     table.auto_set_font_size(False)
     table.set_fontsize(8)
     
     # Uniform cell height
     cellDict = table.get_celld()
     cell_height = 1.0 / (len(rows) + 1)
-    
     for i in range(len(rows) + 1):
         for j in range(len(headers)):
             cellDict[(i, j)].set_height(cell_height)
@@ -59,11 +64,25 @@ def export_spreadsheet_from_csv(csv_path, tournament_name, cs, logo, logo_path, 
             table[(j+1, i)].set_text_props(color=cs["text_color"])
             table[(j+1, i)].set_edgecolor(cs["grid_color"])
     
-    # Save image
-    plt.savefig(
+    # --- Crop exactly to the table bbox ---
+    fig.canvas.draw()
+    renderer = fig.canvas.get_renderer()
+    bbox = table.get_window_extent(renderer)
+    bbox = bbox.transformed(fig.dpi_scale_trans.inverted())
+
+    margin = 0.25  # pouce
+
+    expanded_bbox = bbox.expanded(
+    (bbox.width + 2*margin) / bbox.width,
+    (bbox.height + 2*margin) / bbox.height
+    )
+    
+    # Save image cropped tightly to the table
+    fig.savefig(
         os.path.join("exports", "spreadsheet.png"),
         dpi=spreadsheet_pixel_density,
         facecolor=cs["background_color"],
+        bbox_inches=expanded_bbox,
         pad_inches=0
     )
-    plt.close()
+    plt.close(fig)
